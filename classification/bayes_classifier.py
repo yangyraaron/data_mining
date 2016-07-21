34ba30f61a97cb26b032c3e05659aba810970456
yangyraaron <yangyr.aaron@gmail.com>
2016-07-21 08:56:07 +0800

    finsih bayes classifier

--- classification/bayes_classifier.py
+++ classification/bayes_classifier.py
@@ -31,22 +31,45 @@ class BayesClassifier(object):
                 self._classes.setdefault(category, 0)
                 self._classes[category] += count
                 self._counts.setdefault(category, {})
-                for col, col_value in counts.iteritems():
+                for col, col_value in counts[category].iteritems():
                     self._counts[category].setdefault(col, {})
                     for attr, value in col_value.iteritems():
-                        self._counts[category][col][col_value].setdefault(attr, 0)
-                        self._counts[category][col][col_value][attr] += value   
+                        self._counts[category][col].setdefault(attr, 0)
+                        self._counts[category][col][attr] += value
         
         # compute prior probabilities
         for category, count in self._classes.iteritems():
-            self._prior[category] = count / total   
+            # compute p(h)
+            self._prior[category] = float(count) / self._total   
         # compute conditional probabilities p(h|d)
-        for category, cols in counts.iteritems():
+        for category, cols in self._counts.iteritems():
             self._conditional.setdefault(category, {})
             for col, value_counts in cols.iteritems():
                 self._conditional[category].setdefault(col, {})
                 for attr, count in value_counts.iteritems():
-                    self._conditional[category][col][attr] = count / self._classes[category]             
+                    # compute p(D|h)
+                    self._conditional[category][col][attr] = round(float(count) / self._classes[category], 6)
+
+    def classify(self, item_vector):
+        """
+        classify
+        """
+        results = []
+        for category, prior in self._prior.iteritems():
+            prob = prior
+            col = 1
+            for attr in item_vector:
+                if attr not in self._conditional[category][col]:
+                    # didn't find ansy category instance of this attribute occurring with this category
+                    # prob should be 0
+                    prob = 0
+                else:
+                    prob = prob * self._conditional[category][col][attr]
+                    col += 1
+
+            results.append((round(prob, 6), category))
+        print results
+        return max(results)[1]
 
     def read_data(self, file_path):
         """
@@ -87,4 +110,25 @@ class BayesClassifier(object):
                 counts[category][col][col_value] += 1
         
         return total, classes, counts
-            
+
+
+def test_bayes_classifier():
+    """
+    test bayes classifier
+    """
+    training_file_paths = []
+    for i in xrange(10):
+        index = i + 1
+        file_path = '%s-%02i' % ('data/iHealth/i', index)
+        
+        training_file_paths.append(file_path)
+
+    classifier = BayesClassifier(training_file_paths, '', 'attr attr attr attr class'.split(' '))
+    print 'classifier start training'
+    classifier.training()
+    print 'training complete'
+    print classifier.classify(['health', 'moderate', 'moderate', 'yes'])
+
+
+if __name__ == '__main__':
+    test_bayes_classifier()
